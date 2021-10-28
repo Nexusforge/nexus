b948ea1ff5ebf7a492078c1ca235e72a89042093
Apollo3zehn <vwilms@outlook.com>
2021-10-28 09:12:02 +0200

    Finish licensing.

--- src/Nexus/Shared/ResourceBox.razor.cs
+++ src/Nexus/Shared/ResourceBox.razor.cs
@@ -1,6 +1,12 @@
 ﻿using MatBlazor;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Nexus.Core;
 using Nexus.Services;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
 
 namespace Nexus.Shared
 {
@@ -53,6 +59,15 @@ namespace Nexus.Shared
 		[Inject]
 		private IDatabaseManager DatabaseManager { get; set; }
 
+		[Inject]
+		public AuthenticationStateProvider AuthenticationStateProvider { get; set; }
+
+		[Inject]
+		public ToasterService ToasterService { get; set; }
+
+		[Inject]
+		public UserManager<IdentityUser> UserManager { get; set; }
+
 		public int GroupPageSize { get; set; } = 15;
 
 		public int GroupPage { get; set; } = 0;
@@ -60,6 +75,42 @@ namespace Nexus.Shared
 		public int ResourcePageSize { get; set; } = 9;
 
 		public int ResourcePage { get; set; } = 0;
+
+		#endregion
+
+		#region Methods
+
+		private async Task AcceptLicenseAsync()
+		{
+			var authenticationState = await this.AuthenticationStateProvider.GetAuthenticationStateAsync();
+			var principal = authenticationState.User;
+			var claimType = Claims.CAN_ACCESS_CATALOG;
+
+			if (principal.Identity.IsAuthenticated)
+			{
+				var user = await this.UserManager.GetUserAsync(principal);
+				var claims = await this.UserManager.GetClaimsAsync(user);
+				var claim = claims.FirstOrDefault(claim => claim.Type == claimType);
+				var projectId = this.UserState.CatalogContainer.Id;
+
+				if (claim == null)
+				{
+					var newValue = projectId;
+					claim = new Claim(claimType, newValue);
+					await this.UserManager.AddClaimAsync(user, claim);
+				}
+				else if (!claim.Value.Split(';').Contains(projectId))
+				{
+					var newValue = claim != null
+						? string.Join(';', claim.Value, projectId)
+						: projectId;
+					var newClaim = new Claim(claimType, newValue);
+					await this.UserManager.ReplaceClaimAsync(user, claim, newClaim);
+				}
+			}
+
+			this.ToasterService.ShowSuccess(message: "Please log out and log in again for the changes to take effect.", icon: MatIconNames.Lock_open);
+		}
 
 		#endregion
 	}
